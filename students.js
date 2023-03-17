5d2de441f6328de4bb4e87a2e7cfc701a26b4e02
Teke Byas <tekelamon01@gmail.com>
2023-03-16 19:04:08 -0700

    added my info

--- students.js
+++ students.js
@@ -7,11 +7,11 @@ export const data = [
         imagePath:'images/davidLiang.jpg'
     },
     {
-        firstName:'David',
-        lastName:'Liang',
-        location: 'Philadephia, PA',
-        html:'davidLiang.html',
-        imagePath:'images/davidLiang.jpg'
+        firstName:'Teke',
+        lastName:'Byas',
+        location: 'Rancho Cucamonga, CA',
+        html:'tekebyas.html',
+        imagePath:'images/tekeByas.jpg'
     },
 ]
 
